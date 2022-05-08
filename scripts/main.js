1e612ff2da7c18546de9aa4e7afb1df194b04c8a
froggie4321 <heactora4321@gmail.com>
2022-05-08 13:55:31 -0500

    Just adding linked in link and startingt to remove html hardcoded code. Will use mostly js code to help the site be dynamic and to make updating site easier in the future.

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/*
+BASIC HTML: 
+const h1 = document.createElement("h1");
+const underline = document.createElement("u");
+underline.innerText = "Hello World"
+h1.append(underline);
+document.body.append(h1);
+
+
+MODIFY EXISTING HTML:
+
+
+*/
